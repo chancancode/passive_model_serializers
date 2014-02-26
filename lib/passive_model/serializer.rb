314ec6ef788440b2f31bbbde2f1f311482368fbc
Godfrey Chan <godfreykfc@gmail.com>
2014-02-25 20:00:32 -0800

    Refactored _attributes to store a mapping of attribute name to attribute methods

--- lib/passive_model/serializer.rb
+++ lib/passive_model/serializer.rb
@@ -4,13 +4,13 @@ module PassiveModel
       attr_accessor :_attributes
 
       def inherited(base)
-        base._attributes = (_attributes || []).dup
+        base._attributes = (_attributes || {}).dup
       end
 
       def attributes(*attrs)
-        _attributes.concat attrs
-
         attrs.each do |attr|
+          _attributes[attr] = attr
+
           unless method_defined?(attr)
             define_method attr do
               object.read_attribute_for_serialization attr
@@ -27,13 +27,13 @@ module PassiveModel
     end
 
     def attributes
-      self.class._attributes.dup.each_with_object({}) do |name, hash|
-        hash[name] = send(name)
+      self.class._attributes.dup.each_with_object({}) do |(name, meth), hash|
+        hash[name] = send(meth)
       end
     end
 
     def serializable_hash(options = nil)
       attributes
-    end 
+    end
   end
 end
